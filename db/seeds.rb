08f7e3b025e6751fb82ea6e75768d0f77960df5b
Daniel Stewart <mgdog1@googlemail.com>
2018-05-03 11:38:44 +0100

    student save method created

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+require_relative "../models/student"
+
+#Student.delete_all()
+
+student1 = Student.new({
+  "first_name" => "Harry",
+  "last_name" => "Potter",
+  "house" => "Gryffindor",
+  "age" => "12"
+  })
+
+student2 = Student.new({
+  "first_name" => "Draco",
+  "last_name" => "Malfoy",
+  "house" => "Slytherin",
+  "age" => "13"
+  })
+
+student1.save()
+student2.save()
